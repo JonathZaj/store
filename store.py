a398e51f5d9c1caec441a199321a7584c605ab7d
Jonathan Zajfen <jonathan.zajfen@gmail.com>
2018-12-30 09:33:23 +0200

    mvp

--- store.py
+++ store.py
@@ -5,7 +5,7 @@ import pymysql
 
 connection = pymysql.connect(host="localhost",
                              user="root",
-                             password="0619608644",
+                             password="**********",
                              db="store",
                              charset="utf8",
                              cursorclass=pymysql.cursors.DictCursor)
